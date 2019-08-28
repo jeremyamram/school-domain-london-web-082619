6bbec76fe8e82866ba44deba8de7874096940aa7
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-28 19:19:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+class School
+  attr_reader :roster, :name
+ 
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+   if @roster.include?(grade)
+     @roster[grade] << student
+   else
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+  
+  def grade(grade)
+    grades=[]
+    @roster.each do |key,value|
+      if key == grade
+        grades << value
+      end
+    end
+    return grades.flatten
+  end
+  
+  def sort
+    b = Hash.new {|hash, key| hash[key] = []}
+    @roster.values.each [|e| b[e[]]]
+  end
+end
+    
+    arr = [["a", 1], ["a", 2], [nil, 3], [nil, 4], ["b", 6], ["b", 8]]
+
+h = Hash.new {|hash, key| hash[key] = []}
+arr.each {|e| h[e[0]] << e[1]}
+p h #=> {"a"=>[1, 2], nil=>[3, 4], "b"=>[6, 8]}
+    
+    
+
